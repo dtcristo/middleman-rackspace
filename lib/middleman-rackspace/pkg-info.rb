3cf633947d8e67169e7e2491b014e686acb098c1
David Cristofaro <david@dtcristo.com>
2015-11-10 14:05:26 +1100

    Bump version to 0.3.0

--- lib/middleman-rackspace/pkg-info.rb
+++ lib/middleman-rackspace/pkg-info.rb
@@ -1,7 +1,7 @@
 module Middleman
   module Rackspace
     PACKAGE = 'middleman-rackspace'
-    VERSION = '0.2.0'
+    VERSION = '0.3.0'
     TAGLINE = 'Deploy your Middleman site to Rackspace Cloud Files'
   end
 end
