ea3223cecdba1bcf4c822bdc92bdd45c3abc4cc1
David Cristofaro <david@dtcristo.com>
2015-11-11 13:25:43 +1100

    Add support for git branch switching

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Deploy your [Middleman](https://middlemanapp.com/) site to [Rackspace Cloud Files](http://www.rackspace.com/cloud/files).
 
-# Usage
+## Usage
 
 Add the following to the `Gemfile` for you Middleman project and perform a `bundle install`.
 
@@ -24,7 +24,7 @@ $ middleman rackspace staging
 
 An archive `build_ENVIRONMENT_TIMESTAMP.tar.gz` is created for your current build and pushed to Rackspace. A new container will automatically be configured on the specified region. If there is an existing container with the same name, **data will be overwritten**.
 
-# Configuration
+## Configuration
 
 Activate and configure middleman-rackspace by adding an `activate :rackspace` block to `config.rb`. For example:
 
@@ -36,6 +36,7 @@ activate :rackspace do |config|
   config.rackspace_region     = :syd
   config.container_staging    = 'example-container-staging'
   config.container_production = 'example-container'
+  config.checkout_branch      = true
   config.build_before         = true
 end
 ```
@@ -50,22 +51,26 @@ activate :rackspace do |config|
   config.rackspace_api_key    = ENV['RACKSPACE_API_KEY']
   # The Rackspace region to deploy to
   config.rackspace_region     = :dfw
-  # The target container on Rackspace for staging
+  # The target Rackspace container for each environment
   config.container_staging    = nil
-  # The target container on Rackspace for production
   config.container_production = nil
+  # Git branch for each environment
+  config.branch_staging       = 'staging'
+  config.branch_production    = 'master'
+  # Checkout environment branch before the build/deploy step
+  config.checkout_branch      = false
+  # Run `middleman build` before the deploy step
+  config.build_before         = false
   # Root page filename
   config.index_file           = 'index.html'
   # Error page filename prefix, translates to 401.html and 404.html
   config.error_file_prefix    = '.html'
-  # Run `middleman build` before the deploy step
-  config.build_before         = false
 end
 ```
 
 For a list of available regions see [http://www.rackspace.com/knowledge_center/article/about-regions](http://www.rackspace.com/knowledge_center/article/about-regions).
 
-# Todo
+## Todo
 
 * On deploy, delete files on the server that have been removed from the build.
 * Invalidate cache after deploy.
--- lib/middleman-rackspace/cli.rb
+++ lib/middleman-rackspace/cli.rb
@@ -11,10 +11,14 @@ module Middleman
 
       desc 'rackspace ENVIRONMENT [options]', 'Deploy to Rackspace container for ENVIRONMENT'
 
+      class_option :checkout_branch,
+                   aliases: '-c',
+                   type: :boolean,
+                   desc: 'Checkout environment branch before the build/deploy step'
+
       class_option :build_before,
                    aliases: '-b',
                    type: :boolean,
-                   #default: false,
                    desc: 'Run `middleman build` before the deploy step'
 
       # Tell Thor to exit with a non-zero exit code on failure
@@ -30,14 +34,58 @@ module Middleman
         # Instantiate Middleman and load config
         app = Middleman::Application.server.inst
 
+        # Pull the Rackspace config out of 'app'
+        config = app.extensions[:rackspace].options
+
+        # Checkout branch if specified with '--checkout_branch' or in 'config.rb'
+        original_branch = ''
+        stash = ''
+        checkout_enabled = options[:checkout_branch] || config.checkout_branch
+        switching = false
+        if checkout_enabled
+          original_branch = `git rev-parse --abbrev-ref HEAD`
+          branch = case environment
+                   when 'staging'
+                     config.branch_staging
+                   when 'production'
+                     config.branch_production
+                   end
+
+          switching = original_branch != branch
+          stash = `git stash create`
+          unless stash.empty?
+            puts ''
+            puts 'Working changes have been stashed. On failure, manually restore with:'
+            puts ''
+            puts "    git stash apply #{stash}"
+            puts ''
+          end
+
+          if switching
+            run("git checkout #{branch} --force") || exit(1)
+          elsif !stash.empty?
+            run('git reset --hard') || exit(1)
+          end
+          run('git pull') || exit(1)
+        end
+
         # Run `middleman build` if specified with '-b' or in 'config.rb'
-        build_enabled = options[:build_before] || app.extensions[:rackspace].options.build_before
-        if build_enabled
+        if options[:build_before] || config.build_before
           run('middleman build') || exit(1)
         end
 
         # Deploy the built website
         Middleman::Rackspace.deploy(app, environment)
+
+        # Change back to original branch and restore any saved stash
+        if checkout_enabled
+          unless original_branch.empty?
+            run("git checkout #{original_branch}") || exit(1)
+          end
+          unless stash.empty?
+            run("git stash apply #{stash}") || exit(1)
+          end
+        end
       end
     end
   end
